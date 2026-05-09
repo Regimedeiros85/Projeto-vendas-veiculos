7cea96e4404da63effdf3e909c686036e1172f25
Regimedeiros85 <medeirossilva224@gmail.com>
2026-05-09 18:11:19 -0300

    Adicionar arquivos de projeto.

---
+++ Dominio/Entidades/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Carro : Veiculo
+    {
+        public Carro(int id, string modelo, string marca, int anoFabricacao, string placa) : base(id, modelo, marca, anoFabricacao, placa)
+        {
+        }
+    }
+}
---
+++ Dominio/Entidades/Veiculo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Veiculo
+    {
+        private int Id { get; set; }
+        private string Modelo { get; set; }
+        private string Marca { get; set; }
+        private int AnoFabricacao { get; set; }
+        private string Placa { get; set; }
+        private bool Disponivel { get; set; } = true;
+
+        public Veiculo(int id, string modelo, string marca, int anoFabricacao, string placa)
+        {
+            Id = id;
+            Modelo = modelo;
+            Marca = marca;
+            AnoFabricacao = anoFabricacao;
+            Placa = placa;
+        }
+
+        private void Adicionar()
+        {
+
+        }
+
+        private void Editar()
+        {
+        }
+
+        private void Delete()
+        {
+        }
+
+
+    }
+}
