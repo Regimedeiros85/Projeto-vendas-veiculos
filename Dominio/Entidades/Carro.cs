0f8b1bb53febdb37c658e9272becf63015e5c547
Regimedeiros85 <medeirossilva224@gmail.com>
2026-05-09 18:18:50 -0300

    feat: meu primeiro commit

--- Dominio/Entidades/Carro.cs
+++ Dominio/Entidades/Carro.cs
@@ -11,5 +11,5 @@ namespace Dominio.Entidades
         public Carro(int id, string modelo, string marca, int anoFabricacao, string placa) : base(id, modelo, marca, anoFabricacao, placa)
         {
         }
-    }
+    }// Reginaldo, 2024-06-17
 }
